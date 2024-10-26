d2a24a1237cb48b254e6854783c21d09bfc0f861
Iveta Nováková <novaiv.ivet@gmail.com>
2024-10-26 18:45:44 +0200

    fix cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import avatar from "./images/avatar-iveta.png";
 import { React, useState, useEffect } from "react";
 import MessageFormPopup from "./components/MessageFormPopup.js";
-import { projectFirestore } from "./firebase/config.js"
+import { projectFirestore } from "./firebase/config.js";
 
 const App = () => {
   const [name, setName] = useState("");
@@ -9,29 +9,77 @@ const App = () => {
   const [allMessages, setAllMessages] = useState([]);
   const [error, setError] = useState(false);
   const [messagePopupVisible, setMessagePopupVisible] = useState(false);
+  const [shortestColumn, setShortestColumn] = useState({columnOne: null, columnTwo: null, columnThree: null});
+  const [addMesButtonPos, setAddMesButtonPos] = useState(1);  // Defaultně v prvním sloupci.
 
+  // Nastavení výšky jednotlivých sloupců
+  useEffect(() => {
+    let colOne = 0;
+    let colTwo = 0;
+    let colThree = 0;
+    allMessages.forEach(mes => {
+      if (mes.column === 1) {
+        colOne += (mes.message.length + 80);
+      } else if (mes.column === 2) {
+        colTwo += (mes.message.length + 80);
+      } else {
+        colThree += (mes.message.length + 80);
+      }
+    });
+    setShortestColumn({ columnOne: colOne, columnTwo: colTwo, columnThree: colThree });
+  }, [allMessages]);
+
+  // Logika pro odeslání zprávy
   const sendMessage = async (e) => {
     e.preventDefault();
     if (name && message) {
+      let selectedColumn = 1;
+      if (allMessages.length === 0) {
+        selectedColumn = 1;
+      } else if (allMessages.length === 1) {
+        selectedColumn = 2;
+      } else if (allMessages.length === 2){
+        selectedColumn = 3
+      } else {
+        if ((shortestColumn.columnOne <= shortestColumn.columnTwo) && (shortestColumn.columnOne <= shortestColumn.columnThree)) {
+          selectedColumn = 1;
+        } else if (shortestColumn.columnTwo <= shortestColumn.columnThree) {
+          selectedColumn = 2;
+        } else {
+          selectedColumn = 3;
+        }
+      }
+      let cardIndex = (allMessages.length + 1);
       const currentDate = new Date();
       let minutes = currentDate.getMinutes();
       let doubleDigitMinutes = minutes < 10 ? '0' + minutes : minutes;
-      const formattedDate = `${currentDate.getDate()}.${currentDate.getMonth() + 1}.${currentDate.getFullYear()} ${currentDate.getHours()}:${doubleDigitMinutes}`
-      let newMessage = {name: name, date: formattedDate, message: message}
+      const formattedDate = `${currentDate.getDate()}.${currentDate.getMonth() + 1}.${currentDate.getFullYear()} ${currentDate.getHours()}:${doubleDigitMinutes}`;
+      let newMessage = { name: name, date: formattedDate, message: message, column: selectedColumn, cardIndex: cardIndex };
       try {
-        await projectFirestore.collection("messages").add(newMessage)
-        setMessage("")
-        setName("")
-        setMessagePopupVisible(false)  
-      }catch(err){
-        setError("zpráva nebyla přidána " + err.message)
+        await projectFirestore.collection("messages").add(newMessage);
+        setMessage("");
+        setName("");
+        setMessagePopupVisible(false);
+      } catch (err) {
+        setError("zpráva nebyla přidána " + err.message);
       }
     }
   };
 
+  // Nastavení pozice tlačítka na základě počtu zpráv
+  useEffect(() => {
+    if (allMessages.length === 0) {
+      setAddMesButtonPos(1);  // Zobrazí tlačítko v prvním sloupci
+    } else if (allMessages.length === 1) {
+      setAddMesButtonPos(2);  // Zobrazí tlačítko ve druhém sloupci
+    } else {
+      setAddMesButtonPos(3);  // Zobrazí tlačítko ve třetím sloupci
+    }
+  }, [allMessages]);
+
   const popupVisibility = () => {
     setMessagePopupVisible(false);
-  }
+  };
 
   useEffect(() => {
     const unsubscribe = projectFirestore.collection("messages")
@@ -50,7 +98,19 @@ const App = () => {
   
     return () => unsubscribe();
   }, []);
-  
+
+  useEffect(() => {
+    console.log(shortestColumn.columnOne);
+    console.log(shortestColumn.columnTwo);
+    console.log(shortestColumn.columnThree);
+  }, [shortestColumn]);
+
+  const handleKeyDown = (e) => {
+    if ((e.key === 'Enter') && (message) && (name)) {
+      sendMessage(e);
+    }
+  };
+
   return (
     <div className="app">
       {error && <p>{error}</p>}
@@ -58,56 +118,107 @@ const App = () => {
         <h2>Leave a message</h2>
         <img className="avatar" src={avatar} alt="authors avatar"></img>
       </header>
-      <div className="message-board" >
-        {allMessages.map((singleMessage) => {
-          return (
-            <div className="single-card" key={singleMessage.id}>
-              <div className="miniheader">
-                <p className="name">{singleMessage.name}</p>
-                <p className="date">{singleMessage.date}</p>
+      <div className="message-board">
+        <div className="columnOne">
+          {addMesButtonPos === 1 && <div onClick={() => setMessagePopupVisible(true)} className="single-card add-message">
+            <p>+</p>
+          </div>}
+          {allMessages
+            .filter((oneMessage) => oneMessage.column === 1)
+            .sort((a, b) => a.cardIndex - b.cardIndex) // Řadíme podle cardIndex od nejvyššího k nejnižšímu
+            .map((oneMessage) => (
+              <div className="single-card" key={oneMessage.id}>
+                <div className="miniheader">
+                  <p className="name">{oneMessage.name}</p>
+                  <p className="date">{oneMessage.date}</p>
+                </div>
+                <p className="message">{oneMessage.message}</p>
               </div>
-              <p className="message">{singleMessage.message}</p>
-            </div>
-          );
-        })}
-        <div onClick={()=>setMessagePopupVisible(true)} className="single-card add-message">
-          <p>+</p>
+            ))
+          }
+        </div>
+        <div className="columnTwo">
+          {addMesButtonPos === 2 && <div onClick={() => setMessagePopupVisible(true)} className="add-message single-card">
+            <p>+</p>
+          </div>}
+          {allMessages
+            .filter((oneMessage) => oneMessage.column === 2) 
+            .sort((a, b) => a.cardIndex - b.cardIndex) 
+            .map((oneMessage) => (
+              <div className="single-card" key={oneMessage.id}>
+                <div className="miniheader">
+                  <p className="name">{oneMessage.name}</p>
+                  <p className="date">{oneMessage.date}</p>
+                </div>
+                <p className="message">{oneMessage.message}</p>
+              </div>
+            ))
+          }
+        </div>
+        <div className="columnThree">
+          {allMessages
+            .filter((oneMessage) => oneMessage.column === 3) 
+            .sort((a, b) => a.cardIndex - b.cardIndex) 
+            .map((oneMessage) => (
+              <div className="single-card" key={oneMessage.id}>
+                <div className="miniheader">
+                  <p className="name">{oneMessage.name}</p>
+                  <p className="date">{oneMessage.date}</p>
+                </div>
+                <p className="message">{oneMessage.message}</p>
+              </div>
+            ))
+          }          
+          {addMesButtonPos === 3 && <div onClick={() => setMessagePopupVisible(true)} className="single-card add-message">
+              <p>+</p>
+            </div>}
+        </div>
+        <div className="column-small-devices">
+        {allMessages
+            .sort((a, b) => a.cardIndex - b.cardIndex) 
+            .map((oneMessage) => (
+              <div className="single-card" key={oneMessage.id}>
+                <div className="miniheader">
+                  <p className="name">{oneMessage.name}</p>
+                  <p className="date">{oneMessage.date}</p>
+                </div>
+                <p className="message">{oneMessage.message}</p>
+              </div>
+            ))
+          }          
         </div>
       </div>
       <div className="type-area">
         <form onSubmit={sendMessage} className="message-form">
-          <input onChange={(e) => setName(e.target.value)}
+          <input
+            onChange={(e) => setName(e.target.value)}
             placeholder="your name"
             className="input-name"
             type="text"
             value={name}
           ></input>
-          <input
+          
+          <textarea
             onChange={(e) => setMessage(e.target.value)}
             placeholder="your message"
             className="input-text"
-            type="text"
             value={message}
-          ></input>
+            maxLength={200}
+            onKeyDown={handleKeyDown}
+          ></textarea>
+          
           <input value="send" className="input-submit" type="submit"></input>
         </form>
       </div>
-      {messagePopupVisible && <MessageFormPopup closePopup={popupVisibility} getName ={(e) => setName(e.target.value)} getMessage ={(e) => setMessage(e.target.value)} sendMessage = {sendMessage} nameValue={name} messageValue={message}/>}
+      {messagePopupVisible && <MessageFormPopup closePopup={popupVisibility} getName={(e) => setName(e.target.value)} getMessage={(e) => setMessage(e.target.value)} sendMessage={sendMessage} nameValue={name} messageValue={message} />}
     </div>
   );
 };
 
 export default App;
-// TODO:
-// karty na velkých zařízeních - on hover - ukáže se zbytek + šipka nebo rozostření
-// na malých zař poslední karta nejde vidět (čím menší, tím hůře)
-// info ikona s informacemi
-// při kliknutí na avatar se ukáže info o mně
-// udělat scrollbar atraktivnější
-// když je otevřený MessageFormPopup a zmenší se zařízení na malé, chci aby se popup zavřel a data se přepsaly do "type-area"
-// firebase kapacity - snad vyřešeno, musím počkat na výsledek
 
-// PROJECT INFO:
-// - komentáře od fiktivních filmových postav vygenerovalo AI, zbytek komentářů je od reálných lidí
-// - použito: react, usestate, useeffect, proptypes, firebase, useRef
+// nainstalovat eslint
+// moderní design
+// zeptat se chat gpt jestli je muj kod fajn pro prezentaci
 
+// použito: usestate, useeffect, firebase, node.js
--- src/components/MessageFormPopup.js
+++ src/components/MessageFormPopup.js
@@ -3,6 +3,11 @@ import PropTypes from 'prop-types';
 
 const MessageFormPopup = ({closePopup, getName, getMessage, sendMessage, nameValue, messageValue}) => {
 
+  const handleKeyDown = (e) => {
+    if ((e.key === 'Enter') && (messageValue) && (nameValue)) {
+      sendMessage(e);
+    }
+  };
 
   return (
     <div className='message-form-popup'>
@@ -21,13 +26,46 @@ const MessageFormPopup = ({closePopup, getName, getMessage, sendMessage, nameVal
             type="text"
             value={messageValue}
           ></input>
+          <textarea
+            onChange={getMessage}
+            placeholder="your message"
+            className="input-text"
+            value={messageValue}
+            maxLength={200}
+            onKeyDown={handleKeyDown}
+          ></textarea>
           <input value="send" className="input-submit" type="submit"></input>
         </form>
-        <button onClick={closePopup}>X</button>
+        <button className='closeButton' onClick={closePopup}>X</button>
     </div>
   )
 }
+/*
+      <div className="type-area">
+        <form onSubmit={sendMessage} className="message-form">
+          <input
+            onChange={(e) => setName(e.target.value)}
+            placeholder="your name"
+            className="input-name"
+            type="text"
+            value={name}
+          ></input>
+          
+          <textarea
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="your message"
+            className="input-text"
+            value={message}
+            rows="4"  // nastavení počtu řádků, můžete změnit podle potřeby
+            maxLength={200}
+            onKeyDown={handleKeyDown}
+          ></textarea>
+          
+          <input value="send" className="input-submit" type="submit"></input>
+        </form>
+      </div>
 
+*/
 MessageFormPopup.propTypes = {
   closePopup: PropTypes.func.isRequired,
   getName: PropTypes.func.isRequired,
